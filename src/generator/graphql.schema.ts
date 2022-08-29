927c3da29283381e84afd057b8e8f115d22188ad
Yaxxii1 <maximilianb312@gmx.de>
2022-08-29 19:38:07 +0200

    middle commi

--- src/generator/graphql.schema.ts
+++ src/generator/graphql.schema.ts
@@ -8,33 +8,173 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export enum Gender {
+    UNKNOWN = "UNKNOWN",
+    MALE = "MALE",
+    FEMALE = "FEMALE"
+}
+
+export enum UserType {
+    BASIC = "BASIC",
+    PREMIUM = "PREMIUM"
+}
+
 export class UserInput {
-    username: string;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+    email?: Nullable<string>;
+    password?: Nullable<string>;
+    gender?: Nullable<Gender>;
+}
+
+export class FileInput {
+    filename?: Nullable<string>;
+}
+
+export class RecordInput {
+    User?: Nullable<UserInput>;
+    File?: Nullable<FileInput>;
+}
+
+export class CreateUserInput {
+    firstName: string;
+    lastName: string;
+    email: string;
     password: string;
+    gender: Gender;
+}
+
+export class UpdateUserInput {
+    firstName: string;
+    lastName: string;
+    password: string;
+    gender: Gender;
+}
+
+export class LoginUserInput {
+    email: string;
+    password: string;
+}
+
+export class SearchInput {
+    select?: Nullable<string[]>;
+    where?: Nullable<RecordInput>;
+    start?: Nullable<number>;
+    end?: Nullable<number>;
+    order?: Nullable<JSONObject>;
+    skip?: Nullable<number>;
+    take?: Nullable<number>;
+}
+
+export class Users {
+    users?: Nullable<User[]>;
+}
+
+export class LoginResponse {
+    accessToken: string;
+    refreshToken: string;
+}
+
+export class RefreshTokenResponse {
+    accessToken: string;
+}
+
+export class Local {
+    email?: Nullable<string>;
+    password?: Nullable<string>;
+}
+
+export class Google {
+    _id?: Nullable<string>;
+    token?: Nullable<string>;
+    name?: Nullable<string>;
+    email?: Nullable<string>;
+}
+
+export class Facebook {
+    _id?: Nullable<string>;
+    token?: Nullable<string>;
+    name?: Nullable<string>;
+    email?: Nullable<string>;
 }
 
 export class User {
     _id: string;
-    username: string;
-    password: string;
+    local?: Nullable<Local>;
+    google?: Nullable<Google>;
+    facebook?: Nullable<Facebook>;
+    firstName: string;
+    lastName: string;
+    avatar?: Nullable<string>;
+    gender: Gender;
+    resetPasswordToken?: Nullable<string>;
+    resetPasswordExpires?: Nullable<number>;
+    fullName?: Nullable<string>;
+    isVerified: boolean;
+    isActivated?: Nullable<boolean>;
+    isOnline: boolean;
+    isLocked: boolean;
+    reason: string;
+    isActive: boolean;
+    stripeId?: Nullable<string>;
+    type: UserType;
+    ccLast4?: Nullable<string>;
+    createdAt: number;
+    updatedAt: number;
 }
 
 export abstract class IQuery {
     abstract hello(): string | Promise<string>;
 
-    abstract users(): User[] | Promise<User[]>;
+    abstract me(): Nullable<User> | Promise<Nullable<User>>;
 
-    abstract user(_id: string): User | Promise<User>;
+    abstract users(offset?: Nullable<number>, limit?: Nullable<number>): Nullable<User[]> | Promise<Nullable<User[]>>;
+
+    abstract user(_id: string): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract searchUser(userIds?: Nullable<string[]>): UserResult | Promise<UserResult>;
+
+    abstract today(): Nullable<Date> | Promise<Nullable<Date>>;
 }
 
 export abstract class IMutation {
-    abstract createUser(input: UserInput): User | Promise<User>;
+    abstract createUser(input: CreateUserInput): Nullable<User> | Promise<Nullable<User>>;
 
-    abstract updateUser(_id: string, input: UserInput): User | Promise<User>;
+    abstract updateUser(_id: string, input: UpdateUserInput): Nullable<boolean> | Promise<Nullable<boolean>>;
 
-    abstract deleteUser(_id: string): boolean | Promise<boolean>;
+    abstract updateAvatar(_id: string, file: Upload): Nullable<boolean> | Promise<Nullable<boolean>>;
 
-    abstract deleteAll(): boolean | Promise<boolean>;
+    abstract deleteUser(_id: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract deleteUsers(): boolean | Promise<boolean>;
+
+    abstract verifyEmail(emailToken: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract login(input: LoginUserInput): Nullable<LoginResponse> | Promise<Nullable<LoginResponse>>;
+
+    abstract refreshToken(refreshToken: string): Nullable<RefreshTokenResponse> | Promise<Nullable<RefreshTokenResponse>>;
+
+    abstract lockAndUnlockUser(_id: string, reason: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract changePassword(_id: string, currentPassword: string, password: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract forgotPassword(email: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract resetPassword(resetPasswordToken: string, password: string): Nullable<boolean> | Promise<Nullable<boolean>>;
+
+    abstract createSubscription(source: string, ccLast4: string): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract changeCreditCard(source: string, ccLast4: string): Nullable<User> | Promise<Nullable<User>>;
+
+    abstract validateUser(text: string, input: CreateUserInput): Nullable<boolean> | Promise<Nullable<boolean>>;
 }
 
+export abstract class ISubscription {
+    abstract newUser(): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export type JSON = any;
+export type JSONObject = any;
+export type Upload = any;
+export type UserResult = User | Users;
 type Nullable<T> = T | null;
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,30 +1,53 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-import { UserService } from './user.service';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ForbiddenError, ApolloError } from 'apollo-server-express';
+import { MongoRepository } from 'typeorm';
 import { User, UserInput } from './user.model';
-import { asyncScheduler } from 'rxjs/internal/scheduler/async';
+import { UserService } from './user.service';
 
 @Resolver('User')
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    @InjectRepository(User)
+    private readonly userRepository: MongoRepository<User>,
+  ) {}
 
   @Query(() => String)
   async hello() {
     return 'world';
   }
 
+  // @Query(() => [User])
+  // async users() {
+  //   return this.userService.findAll();
+  // }
+
   @Query(() => [User])
   async users() {
-    return this.userService.findAll();
+    return this.userRepository.find();
   }
 
   @Query(() => User)
   async user(@Args('_id') _id: string) {
-    return this.userService.findById(_id);
+    try {
+      const user = await this.userRepository.manager
+        .getMongoRepository(User)
+        .findOne({ where: { _id } });
+      if (!user) {
+        throw new ForbiddenError('User not found');
+      }
+      return user;
+    } catch (error) {
+      throw new ApolloError(error);
+    }
   }
 
   @Mutation(() => User)
   async createUser(@Args('input') input: UserInput) {
-    return await this.userService.create(input);
+    try {
+      const { password, username } = input;
+    } catch (error) {}
   }
 
   @Mutation(() => Boolean)
@@ -32,9 +55,14 @@ export class UserResolver {
     return await this.userService.update(_id, input);
   }
 
+  // @Mutation(() => Boolean)
+  // async deleteUser(@Args('_id') _id: string) {
+  //   return await this.userService.delete(_id);
+  // }
+
   @Mutation(() => Boolean)
   async deleteUser(@Args('_id') _id: string) {
-    return await this.userService.delete(_id);
+    return await this.userRepository.delete(_id);
   }
 
   @Mutation(() => Boolean)
